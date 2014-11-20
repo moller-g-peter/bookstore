09c9a03adcd9a1cd8bb3b00a17a59d8648e3018b
miloroot <milonice@hotmail.com>
2014-11-20 14:18:55 +0100

    Added classes to the <input>'s so we can fetch them in jQuery easy. Grabbed the <input>'s in main.js with a console.log for testing.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(function(){
 
+	$('.ISBN').click(function(){
+		console.log('Search via ISBN.');
+	})
+
+	$('.price').click(function(){
+		console.log('Add Price.');
+	})
+
+	$('.quantity').click(function(){
+		console.log('Add Quantity.');
+	})
+
+	$('.daytime').click(function(){
+		console.log('Add date of arrival.');
+	})
+
+	$('.bookshelf').click(function(){
+		console.log('Add place in shelf.');
+	})
 
 });
