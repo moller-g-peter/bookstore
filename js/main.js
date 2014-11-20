fad3e20f1ac03282008bc8ac43577f6ac726dea9
Dennislampert <dennis@lampert.se>
2014-11-20 18:12:48 +0100

    logiteck

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@ $(function(){
 	function forStaff(){
 		$('.ISBN').click(function(){
 			console.log('Search via ISBN.');
-		})
+		});
 
 		$('.price').click(function(){
 			console.log('Add Price.');
-		})
+		});
 
 		$('.quantity').click(function(){
 			console.log('Add Quantity.');
-		})
+		});
 
 		$('.daytime').click(function(){
 			console.log('Add date of arrival.');
-		})
+		});
 
 		$('.bookshelf').click(function(){
 			console.log('Add place in shelf.');
-		})
-	};
+		});
+	}
 
 	forStaff();
 
