97f444758df7e27067b1fa11af6b2d1842b4d926
Dennislampert <dennis@lampert.se>
2014-11-21 15:53:30 +0100

    Tryed deeper wit the ajax..

--- js/main.js
+++ js/main.js
@@ -1,58 +1,61 @@
 $(function(){
 
-	function sendisbn(){
-			$('.boom').click(function(free){
-			console.log('Search via ISBN.', free);
-				function userLogin() {
-					$.ajax({
-					// Use Nodebite's magic library
-					url:"libs/sql-ajax-json.php",
-						// Expect json in return
-						dataType: "json",
-						data: {
-							// Read SQL questions from this file
-							sql: "sql/product-questions.sql",
-							// Run the query named all products
-							run: "book input",
-							//data to send
-							isbn: JSON.stringify(["isbn"]),
-							
-						},
-						// When we have got an response from the server
-						// run something
-						success: function(data) {
-							console.log("4. userLogin success: ", data);
-							console.log("now calling processLogin()...");
-							
-						}
-					});
-				}
+
+
+	$('.boom').click(function(){
+		sendIsbn();
+	});
+	function sendIsbn() {
+
+		var isbn = $('.isbn').val();
+		console.log("isbn: ",isbn);
+
+
+		$.ajax({
+		// Use Nodebite's magic library
+		url:"libs/sql-ajax-json.php",
+			// Expect json in return
+			dataType: "json",
+			data: {
+				// Read SQL questions from this file
+				sql: "sql/product-questions.sql",
+				// Run the query named all products
+				run: "book input",
+				//data to send
+				isbn: JSON.stringify(["isbn"]),
+			},
+			// When we have got an response from the server
+			// run something
+			success: function(data) {
+				console.log("sendIsbn success: ", data);
+			}
 		});
 	}
 
-	function forStaff(){
 
-		$('.price').click(function(){
-			console.log('Add Price.');
-		});
+	// function forStaff(){
 
-		$('.quantity').click(function(){
-			console.log('Add Quantity.');
-		});
+	//	$('.price').click(function(){
+	//		console.log('Add Price.');
+	//	});
 
-		$('.daytime').click(function(){
-			console.log('Add date of arrival.');
-		});
+	//	$('.quantity').click(function(){
+	//		console.log('Add Quantity.');
+	//	});
 
-		$('.bookshelf').click(function(){
-			console.log('Add place in shelf.');
-		});
+	//	$('.daytime').click(function(){
+	//		console.log('Add date of arrival.');
+	//	});
 
-		$('.manualPrice').click(function(){
-			console.log('Change the price manualy.');
-		});
-	}
+	//	$('.bookshelf').click(function(){
+	//		console.log('Add place in shelf.');
+	//	});
 
-	forStaff();
+	//	$('.manualPrice').click(function(){
+	//		console.log('Change the price manualy.');
+	//	});
+	// }
+
+	// forStaff();
 
 });
--- sql/product-questions.sql
+++ sql/product-questions.sql
@@ -2,7 +2,7 @@
 # hostname: 127.0.0.1
 # username: root
 # password: mysql
-# database: webshop
+# database: bookstore
 
 # get categories
 SELECT * FROM pcategories;
