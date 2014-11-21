d239f3f2532f10566844a7262e33eb4f0bce64e1
miloroot <milonice@hotmail.com>
2014-11-21 17:43:11 +0100

    Added submit() to ajax(). Info written in <input>'s now appear in console.log.

--- js/main.js
+++ js/main.js
@@ -2,37 +2,47 @@ $(function(){
 
 
 
-	$('.boom').click(function(){
-		userLogin();
-	});
-	function userLogin() {
+	//$('.boom').click(function(){
+	//	userLogin();
+	//});
 
-		var isbn = $('.isbn').val();
-		console.log("isbn: ",isbn);
+	$('.staffForm').submit(function() {
+		var bookInfo = {};
 
+		$(this).find("input").not("input[type='submit']").each(function() {
+			bookInfo[this.name] = $(this).val();
+    	});
 
-		$.ajax({
-		// Use Nodebite's magic library
-		url:"libs/sql-ajax-json.php",
-			// Expect json in return
-			dataType: "json",
-			data: {
-				// Read SQL questions from this file
-				sql: "sql/product-questions.sql",
-				// Run the query named all products
-				run: "book input",
-				//data to send
-				isbn: JSON.stringify(["isbn"]),
-				
-			},
-			// When we have got an response from the server
-			// run something
-			success: function(data) {
-				console.log("4. userLogin success: ", data);
-			}
-		});
+    	console.log("bookInfo: ", bookInfo);
+
+		//function userLogin(data) {
+
+			//var isbn = $('.isbn').val();
+			//console.log("isbn: ",isbn);
+
+			$.ajax({
+			// Use Nodebite's magic library
+			url:"libs/sql-ajax-json.php",
+				// Expect json in return
+				dataType: "json",
+				data: {
+					// Read SQL questions from this file
+					sql: "sql/product-questions.sql",
+					// Run the query named all products
+					run: "book input",
+					//data to send
+					isbn: JSON.stringify(bookInfo["isbn"])
+					
+				},
+				// When we have got an response from the server
+				// run something
+				success: function(data) {
+					console.log("4. userLogin success: ", data, bookInfo);
+				}
+			});
+		//}
 		return false;
-	}
+	});
 
 
 	function forStaff(){
