74aec26e69a6e6a1b86acba32aa44ae301e22012
Dennislampert <dennis@lampert.se>
2014-11-21 14:21:43 +0100

    still building ajax

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 $(function(){
 
 	function forStaff(){
-		$('.ISBN').click(function(){
+
+		$('.ISBN').submit(function(){
 			console.log('Search via ISBN.');
+				function userLogin() {
+					$.ajax({
+					// Use Nodebite's magic library
+					url:"libs/sql-ajax-json.php",
+						// Expect json in return
+						dataType: "json",
+						data: {
+							// Read SQL questions from this file
+							sql: "sql/product-questions.sql",
+							// Run the query named all products
+							run: "book input",
+							//data to send
+							isbn: JSON.stringify(["isbn"]),
+						 
+						},
+						// When we have got an response from the server
+						// run something
+						success: function(data) {
+							console.log("4. userLogin success: ", data);
+							console.log("now calling processLogin()...");
+							userLogin(data);
+						}
+					});
+				}
 		});
 
 		$('.price').click(function(){
@@ -25,7 +50,32 @@ $(function(){
 			console.log('Change the price manualy.');
 		});
 	}
-	
+
 	forStaff();
 
 });
+
+  function userLogin() {
+    $.ajax({
+      // Use Nodebite's magic library
+      url:"libs/sql-ajax-json.php",
+      // Expect json in return
+      dataType: "json",
+      data: {
+        // Read SQL questions from this file
+        sql: "sql/product-questions.sql",
+        // Run the query named all products
+        run: "book input",
+        //data to send
+        isbn: JSON.stringify(["isbn"]),
+       
+      },
+      // When we have got an response from the server
+      // run something
+      success: function(data) {
+        console.log("4. userLogin success: ", data);
+        console.log("now calling processLogin()...");
+        userLogin(data);
+      }
+    });
+  }
