9c3d008da1de967d5983497f7cbc6b05d4971036
miloroot <milonice@hotmail.com>
2014-11-21 19:28:38 +0100

    Fixed typos in index.html's <input>'s name="", so they are the same as in JS, SQL and in the DB. Added all <input>'s in the ajax().

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $(function(){
 
-
-
-	//$('.boom').click(function(){
-	//	userLogin();
-	//});
-
 	$('.staffForm').submit(function() {
 		var bookInfo = {};
 
@@ -15,11 +9,6 @@ $(function(){
 
 			console.log("bookInfo: ", bookInfo);
 
-		//function userLogin(data) {
-
-			//var isbn = $('.isbn').val();
-			//console.log("isbn: ",isbn);
-
 			$.ajax({
 			// Use Nodebite's magic library
 			url:"libs/sql-ajax-json.php",
@@ -28,22 +17,24 @@ $(function(){
 				data: {
 					// Read SQL questions from this file
 					sql: "sql/product-questions.sql",
-					// Run the query named all products
+					// Run the query named book input
 					run: "book input",
 					//data to send
+				data: {					
+					sql: "sql/product-questions.sql",				
+					run: "book input",					
 					isbn: JSON.stringify(bookInfo["isbn"]),
 					fPrice: JSON.stringify(bookInfo["fPrice"]),
 					amount: JSON.stringify(bookInfo["amount"]),
 					shelf: JSON.stringify(bookInfo["shelf"])
-
 				},
-				// When we have got an response from the server
-				// run something
 				success: function(data) {
-					console.log("userLogin success: ", data, bookInfo);
-				}
+					console.log("Add bookInfo success: ", data, bookInfo);
+				},
+				error: function(data) {
+        			console.log("error: ", data);
+      			}
 			});
-		//}
 		return false;
 	});
 
@@ -72,39 +63,4 @@ $(function(){
 	}
 
 	forStaff();
-
-
-
-
-		
-			// $('.boom').click(function(){
-			//	function isbnLogin() {
-			//		console.log("qwerty");
-			//		$.ajax({
-			//				// Use Nodebite's magic library
-			//				url:"libs/sql-ajax-json.php",
-			//				// Expect json in return
-			//				dataType: "json",
-			//				data: {
-			//				// Read SQL questions from this file
-			//				sql: "sql/product-questions.sql",
-			//				// Run the query named all products
-			//				run: "book input",
-			//				//data to send
-			//				isbn: JSON.stringify(isbnLogin["isbn"])
-								
-			//					},
-			//					// When we have got an response from the server
-			//					// run something
-			//					success: function(data) {
-			//						console.log("4. userLogin success: ", data);
-			//						console.log("now calling processLogin()...");
-							
-			//					}
-			//			});
-
-			//		}
-			// });
-	
-
 });
