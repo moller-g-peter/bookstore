818479b9e1e5c494a20ada371a66cd2a6f8e2199
Dennislampert <dennis@lampert.se>
2014-11-21 14:42:08 +0100

    sharing my thoughts

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(function(){
 
-	function forStaff(){
-
-		$('.ISBN').submit(function(){
-			console.log('Search via ISBN.');
+	function sendisbn(){
+			$('.boom').click(function(free){
+			console.log('Search via ISBN.', free);
 				function userLogin() {
 					$.ajax({
 					// Use Nodebite's magic library
@@ -17,18 +16,21 @@ $(function(){
 							run: "book input",
 							//data to send
 							isbn: JSON.stringify(["isbn"]),
-						 
+							
 						},
 						// When we have got an response from the server
 						// run something
 						success: function(data) {
 							console.log("4. userLogin success: ", data);
 							console.log("now calling processLogin()...");
-							userLogin(data);
+							
 						}
 					});
 				}
 		});
+	}
+
+	function forStaff(){
 
 		$('.price').click(function(){
 			console.log('Add Price.');
@@ -54,28 +56,3 @@ $(function(){
 	forStaff();
 
 });
-
-  function userLogin() {
-    $.ajax({
-      // Use Nodebite's magic library
-      url:"libs/sql-ajax-json.php",
-      // Expect json in return
-      dataType: "json",
-      data: {
-        // Read SQL questions from this file
-        sql: "sql/product-questions.sql",
-        // Run the query named all products
-        run: "book input",
-        //data to send
-        isbn: JSON.stringify(["isbn"]),
-       
-      },
-      // When we have got an response from the server
-      // run something
-      success: function(data) {
-        console.log("4. userLogin success: ", data);
-        console.log("now calling processLogin()...");
-        userLogin(data);
-      }
-    });
-  }
