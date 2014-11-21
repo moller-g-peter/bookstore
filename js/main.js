b1d6cab80a620a8aa5ad79630a0514deb81cd4c2
Dennislampert <dennis@lampert.se>
2014-11-21 19:03:57 +0100

    Added some vals to send with ajax

--- js/main.js
+++ js/main.js
@@ -11,26 +11,39 @@ $(function(){
 
 		$(this).find("input").not("input[type='submit']").each(function() {
 			bookInfo[this.name] = $(this).val();
-    	});
+		});
 
-    	console.log("bookInfo: ", bookInfo);
+			console.log("bookInfo: ", bookInfo);
 
+		//function userLogin(data) {
+
+			//var isbn = $('.isbn').val();
+			//console.log("isbn: ",isbn);
 
 			$.ajax({
-			url:"libs/sql-ajax-json.php",			
+			// Use Nodebite's magic library
+			url:"libs/sql-ajax-json.php",
+				// Expect json in return
 				dataType: "json",
-				data: {					
-					sql: "sql/product-questions.sql",				
-					run: "book input",					
-					isbn: JSON.stringify(bookInfo["isbn"])					
+				data: {
+					// Read SQL questions from this file
+					sql: "sql/product-questions.sql",
+					// Run the query named all products
+					run: "book input",
+					//data to send
+					isbn: JSON.stringify(bookInfo["isbn"]),
+					fPrice: JSON.stringify(bookInfo["fPrice"]),
+					amount: JSON.stringify(bookInfo["amount"]),
+					shelf: JSON.stringify(bookInfo["shelf"])
+
 				},
 				// When we have got an response from the server
 				// run something
 				success: function(data) {
-					console.log("4. userLogin success: ", data, bookInfo);
+					console.log("userLogin success: ", data, bookInfo);
 				}
 			});
-	
+		//}
 		return false;
 	});
 
@@ -60,5 +73,38 @@ $(function(){
 
 	forStaff();
 
+
+
+
+		
+			// $('.boom').click(function(){
+			//	function isbnLogin() {
+			//		console.log("qwerty");
+			//		$.ajax({
+			//				// Use Nodebite's magic library
+			//				url:"libs/sql-ajax-json.php",
+			//				// Expect json in return
+			//				dataType: "json",
+			//				data: {
+			//				// Read SQL questions from this file
+			//				sql: "sql/product-questions.sql",
+			//				// Run the query named all products
+			//				run: "book input",
+			//				//data to send
+			//				isbn: JSON.stringify(isbnLogin["isbn"])
+								
+			//					},
+			//					// When we have got an response from the server
+			//					// run something
+			//					success: function(data) {
+			//						console.log("4. userLogin success: ", data);
+			//						console.log("now calling processLogin()...");
+							
+			//					}
+			//			});
+
+			//		}
+			// });
+	
 
 });
