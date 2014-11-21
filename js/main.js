67bd994de0a32663fa075c018889d1e3505d46ae
Dennislampert <dennis@lampert.se>
2014-11-21 16:55:17 +0100

    Exedantly removed peters code. Its back with a comment.

--- js/main.js
+++ js/main.js
@@ -60,4 +60,38 @@ $(function(){
 
 	forStaff();
 
+
+
+
+		
+			// $('.boom').click(function(){
+			//	function isbnLogin() {
+			//		console.log("qwerty");
+			//		$.ajax({
+			//				// Use Nodebite's magic library
+			//				url:"libs/sql-ajax-json.php",
+			//				// Expect json in return
+			//				dataType: "json",
+			//				data: {
+			//				// Read SQL questions from this file
+			//				sql: "sql/product-questions.sql",
+			//				// Run the query named all products
+			//				run: "book input",
+			//				//data to send
+			//				isbn: JSON.stringify(isbnLogin["isbn"])
+								
+			//					},
+			//					// When we have got an response from the server
+			//					// run something
+			//					success: function(data) {
+			//						console.log("4. userLogin success: ", data);
+			//						console.log("now calling processLogin()...");
+							
+			//					}
+			//			});
+
+			//		}
+			// });
+	
+
 });
