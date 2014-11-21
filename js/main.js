27be10f0d3a51ccb28e4a3276067bcd35ff1d856
moller.peter <moller.g.peter@gmail.com>
2014-11-21 15:54:47 +0100

    försöker lösa gåtan med ajax

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
 $(function(){
 
 
+	// $('.boom').click(function(){
+	// 	sendIsbn();
+	// });
+	// function sendIsbn() {
 
-	$('.boom').click(function(){
-		sendIsbn();
-	});
-	function sendIsbn() {
-
-		var isbn = $('.isbn').val();
-		console.log("isbn: ",isbn);
+	// 	var isbn = $('.isbn').val();
+	// 	console.log("isbn: ",isbn);
 
 
-		$.ajax({
-		// Use Nodebite's magic library
-		url:"libs/sql-ajax-json.php",
-			// Expect json in return
-			dataType: "json",
-			data: {
-				// Read SQL questions from this file
-				sql: "sql/product-questions.sql",
-				// Run the query named all products
-				run: "book input",
-				//data to send
-				isbn: JSON.stringify(["isbn"]),
-			},
-			// When we have got an response from the server
-			// run something
-			success: function(data) {
-				console.log("sendIsbn success: ", data);
-			}
-		});
-	}
+	// 	$.ajax({
+	// 	// Use Nodebite's magic library
+	// 	url:"libs/sql-ajax-json.php",
+	// 		// Expect json in return
+	// 		dataType: "json",
+	// 		data: {
+	// 			// Read SQL questions from this file
+	// 			sql: "sql/product-questions.sql",
+	// 			// Run the query named all products
+	// 			run: "book input",
+	// 			//data to send
+	// 			isbn: JSON.stringify(["isbn"]),
+	// 		},
+	// 		// When we have got an response from the server
+	// 		// run something
+	// 		success: function(data) {
+	// 			console.log("sendIsbn success: ", data);
+	// 		}
+	// 	});
+	// }
 
 
 	// function forStaff(){
@@ -54,6 +53,60 @@ $(function(){
 	//	$('.manualPrice').click(function(){
 	//		console.log('Change the price manualy.');
 	//	});
+
+		
+			$('.boom').click(function(){
+					function isbnLogin() {
+						console.log("qwerty");
+						$.ajax({
+								// Use Nodebite's magic library
+								url:"libs/sql-ajax-json.php",
+								// Expect json in return
+								dataType: "json",
+								data: {
+								// Read SQL questions from this file
+								sql: "sql/product-questions.sql",
+								// Run the query named all products
+								run: "book input",
+								//data to send
+								isbn: JSON.stringify(isbnLogin["isbn"])
+								
+								},
+								// When we have got an response from the server
+								// run something
+								success: function(data) {
+									console.log("4. userLogin success: ", data);
+									console.log("now calling processLogin()...");
+								
+								}
+						});
+
+					}
+			});
+		
+	
+
+	// function forStaff(){
+
+	// 	$('.price').click(function(){
+	// 		console.log('Add Price.');
+	// 	});
+
+	// 	$('.quantity').click(function(){
+	// 		console.log('Add Quantity.');
+	// 	});
+
+	// 	$('.daytime').click(function(){
+	// 		console.log('Add date of arrival.');
+	// 	});
+
+	// 	$('.bookshelf').click(function(){
+	// 		console.log('Add place in shelf.');
+	// 	});
+
+	// 	$('.manualPrice').click(function(){
+	// 		console.log('Change the price manualy.');
+	// 	});
 	// }
 
 	// forStaff();
