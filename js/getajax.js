5d0844f03a30d6db7376d707c4e40170a8646191
Dennislampert <dennis@lampert.se>
2014-11-24 20:24:56 +0100

    This is the new ajax without result

--- js/getajax.js
+++ js/getajax.js
@@ -1,21 +1,35 @@
 $(function(){
-function getbooks(){
-    $.ajax({
-      // Use Nodebite's magic library
-      url:"libs/sql-ajax-json.php",
-      // Expect json in return
-      dataType: "json",
-      data: {
-        // Read SQL questions from this file
-        sql: "sql/product-questions.sql",
-        // Run the query named all products
-        run: "get books by isbn"
-      },
-      // When we have got an response from the server
-      // run listAllProducts
-      success:function(data);
+
+  $('.searchForm').submit(function() {
+    var bookInfo = {};
+
+    $(this).find("input").not("input[type='submit']").each(function() {
+      bookInfo[this.name] = $(this).val();
     });
-  }
+      $.ajax({
+      url:"libs/sql-ajax-json.php",
+        dataType: "json",
+        data: {
+          sql: "sql/product-questions.sql",
+          run: "get books by isbn",
+          isbn: JSON.stringify(bookInfo["isbn"])
+        },
+        success: function(data) {
+          console.log("Add bookInfo success bookInfo: ", bookInfo);
+           console.log("Add bookInfo success data: ", data);
+          // alert("You have succefully stored your data!");
+          $('.resultWindow').append('<p class="p1">' + 'You added: ' + '<b>' + data.title + '</b>' + '. By author: ' + '<b>' + data.author + '.</b>' + ' Amount added: ' + '<b>' + data.amount + '.</b>' + '<br>' + 
+            'Added to shelf: ' + '<b>' + data.shelf + '.</b>');
+          $(':input', '.inputForm').val('');
+        },
+        error: function(data) {
+          console.log("error: ", data);
+          alert("Fill in all input fields.");
+          }
+      });
+    return false;
+  });
+  
 
 
 
