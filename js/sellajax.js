4a569ccdeef39f8937ef1535022aaa97c0133412
miloroot <milonice@hotmail.com>
2014-11-25 11:46:34 +0100

    Books being sold now are stored in booklog-table. ISBN and amount-sold is being sent/stored.

--- js/sellajax.js
+++ js/sellajax.js
@@ -37,6 +37,41 @@ $(function(){
       });
     return false;
   });
+
+  $('.makeSale').submit(function() {
+    var sellBook = {};
+
+    $(this).find("input").not("input[type='submit']").each(function() {
+      sellBook[this.name] = $(this).val();
+    });
+
+    $.ajax({
+      url:"libs/sql-ajax-json.php",
+        dataType: "json",
+        data: {
+          sql: "sql/product-questions.sql",
+          run: "update books",
+          isbnLog: JSON.stringify(sellBook["isbnLog"]),
+          amountLog: JSON.stringify(sellBook["amountLog"])
+        },
+        success: function(data) {
+          $('.resultWindow').html("");
+          var resultHtml = $('.resultWindow');
+          console.log("Add sellBook success: ", sellBook.data);
+          console.log("Add sellBook success data: ", data);
+          // alert("You have succefully stored your data!");
+          for (var i = 0; i < data.length; i++) {
+            var article = $('<article class="p1"/>');
+            article.append('<h2>' + 'You sold book with ISBN: ' + data[i].isbn +  ". " + 'This amount of copies: ' + data[i].amount + '.</h2>');
+            resultHtml.append(article);
+          }
+        },
+        error: function(data) {
+          console.log("error: ", data);
+        }
+      });
+    return false;
+  });
 });
 
 
