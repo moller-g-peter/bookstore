ce5da4cf72987bdf4dba66746a5bfbdb67872774
moller-g-peter <=>
2014-12-07 17:55:14 +0100

    changed question

--- sql/product-questions.sql
+++ sql/product-questions.sql
@@ -28,7 +28,7 @@ SELECT users.uid, users.fname, users.lname, users.email FROM users, login WHERE 
 INSERT INTO booklist (isbn, title, author, fPrice, amount, shelf) VALUES ({isbn}, {title}, {author}, {fPrice}, {amount}, {shelf});
 
 # get books by isbn
-SELECT * FROM pricelist WHERE isbn={isbn};
+SELECT * FROM booklist WHERE isbn={isbn};
 
 #get info for customer by isbn
 SELECT * FROM customer_info where isbn={isbn};
